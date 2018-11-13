7d87418797ab531f5267ad34d92ba116e2cd384c
andi064 <andi064@gmail.com>
2018-11-13 16:26:31 +0100

    all new htmls and filter ready

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 // Main funtion to be called for display information 
-table(data.results[0].members);  
 states(); //allways called for the states to show on the dropdown filter
 
-//-------------------------------------------------------------------------------------------------------//
+//-----------------------------------------------------------------------------------------------------------------------------------------------------//
 
 
 function states() { // function to display all the states on the filter dropdown list 
@@ -25,93 +24,83 @@ function states() { // function to display all the states on the filter dropdown
 	for (var ll = 0; ll < repeatedValues.length; ll++) { // new loop to append every state to the selection / option html
 		let option = document.createElement("option");
 		option.append(repeatedValues[ll]);
+
 		selectOpt.append(option);
 	}
 }
-
 //-------------------------------------------------------------------------------------------------------//
 
-
-//--------------------------------States array that calles the table ------------------------------------//
-
+document.getElementById("selectOpt").addEventListener("change", filterAll); // event that listens to the filter drop down list and starts the function filterAll
 
 
-
-function partyFilter() { //This function is to get all the checkboxes with query selector
-	let partyName = [];
-	let checkValue = document.querySelectorAll("input[name=partyName]:checked"); // gets the values of all checkboxes and stores them in partyName array 
-	for (let i = 0; i < checkValue.length; i++) {
-		partyName.push(checkValue[i].value);
-	}
-	partyFilterTable(partyName); // thats how u call a previous function to get all the var.
-}
-
-function partyFilterTable(partyname) {
-	let filterParty = [];
-	if (partyname.length == 0) {
-		filterParty = data.results[0].members;
-	} else {
-		for (var i = 0; i < data.results[0].members.length; i++) {
-			for (var a = 0; a < partyname.length; a++) {
-				if (data.results[0].members[i].party == partyname[a]) {
-					filterParty.push(data.results[0].members[i]);
+function filterAll() {
+	var members = data.results[0].members; // insted of writing everything I just write members.length etc for the loops 
+	let checkValue = [...document.querySelectorAll("input[name=partyName]:checked")]; // gets the values of all checkboxes and stores them
+	let parties = checkValue.map(checked => checked.value); // a function that filters every value of the checkboxes and stores the value in an array
+	let idVal = document.getElementById("selectOpt"); // connect the variable to the value of the html checkbox "value=D,R,I" 
+	let statesF = idVal.value; // the value of the filter by state
+	let filteredMembers = []; // main array that gets filtered by the loops and used to store elements every time a condition is met
+	
+	for (var i = 0; i < members.length; i++) { // main loop that is going to be used by every if(conditions are met / not )
+		if ((statesF == "All") && (parties.length == 0)) { // if the states dropdown has no value and no checkboxes checked condition
+			filteredMembers.push(members[i]); // Loop everything and push all members to the main array
+		} else if ((members[i].state == statesF) && (parties.length == 0)) { // if members state is true and no checkboxes are checked 
+			filteredMembers.push(members[i]);
+		} else if ((statesF == "All") && (parties.length > 0)) { // if state is false and the checkboxes are checked 
+			for (var i = 0; i < members.length; i++) { // loop through all the members 
+				for (var a = 0; a < parties.length; a++) {// loop to filter all the members by the checkbox value  
+					if (members[i].party === parties[a]) { // if members party is equal to the value of checkbox 
+						filteredMembers.push(members[i]); // push everything to the array 
+					}
+				}
+			}
+		} else if ((statesF == members[i].state) && (parties.length > 0)) { // if state value is true and checkboxes checked 
+			for (var i = 0; i < members.length; i++) { 
+				for (var a = 0; a < parties.length; a++) { //loop through the value of the checkboxes
+					if ((members[i].party == parties[a]) && (members[i].state == statesF)) { //if value of states and value of party is equal to the member
+						filteredMembers.push(members[i]);
+					}
 				}
 			}
 		}
+		table(filteredMembers); // call the table with this array to print the results 
 	}
-	console.log("filterPArty", filterParty)
-	stateFilter(filterParty);
-//	table(filterParty); // call the table function to generate a new table with the value of the checkboxes
 }
+	filterAll(); // Needs to be called to print all the members without filters 
 
-document.getElementById("selectOpt").addEventListener("change",stateFilter);
+	//-------------------------------------------------------------------------------------------------------//
 
-function stateFilter (filterParty){
-	let stateFiltered = [];
-	let idVal = document.getElementById("selectOpt");
-	let statesF = idVal.value;
-	for ( let i = 0 ; i < filterParty.length; i++){
-		if(statesF === filterParty[i].state){
-			stateFiltered.push(filterParty[i]);
+
+	function table(stateFiltered) { //displays string data as a table by creating td & tr
+		let tbody = document.getElementById("tbody");
+		tbody.innerHTML = ''; // the div .tbody displays empty 
+		for (var a = 0; a < stateFiltered.length; a++) { //loop the array
+			let tr = document.createElement("tr"); //define the new variables for fn
+			let td = document.createElement("td");
+			let td1 = document.createElement("td");
+			let td2 = document.createElement("td");
+			let td3 = document.createElement("td");
+			let td4 = document.createElement("td");
+			let name = stateFiltered[a].first_name; // give values to the variables
+			let surname = stateFiltered[a].last_name;
+			let middleName = stateFiltered[a].middle_name || " "; // means or else 
+			let party = stateFiltered[a].party;
+			let state = stateFiltered[a].state;
+			let seniority = stateFiltered[a].seniority;
+			let voteperc = stateFiltered[a].votes_with_party_pct;
+			let anchor = document.createElement("a"); //this creates the anchor elemen
+			let fullName = name + " " + middleName + " " + surname;
+			let linkName = stateFiltered[a].url; // link variable finds the value
+			anchor.setAttribute("href", linkName);
+			anchor.setAttribute("target", "_blank");
+			anchor.innerHTML = fullName;
+			td.append(anchor); // used to join different values "conct"
+			td1.append(party);
+			td2.append(state);
+			td3.append(seniority);
+			td4.append(voteperc + "%");
+			tr.append(td, td1, td2, td3, td4);
+			tbody.append(tr);
 		}
+
 	}
-	console.log(stateFiltered);
-	table(stateFiltered);
-}
-
-//-------------------------------------------------------------------------------------------------------//
-
-
-function table(stateFiltered) { //displays string data as a table by creating td & tr
-	let tbody = document.getElementById("tbody");
-	tbody.innerHTML = ''; // the div .tbody displays empty 
-	for (var a = 0; a < stateFiltered.length; a++) { //loop the array
-		let tr = document.createElement("tr"); //define the new variables for fn
-		let td = document.createElement("td");
-		let td1 = document.createElement("td");
-		let td2 = document.createElement("td");
-		let td3 = document.createElement("td");
-		let td4 = document.createElement("td");
-		let name = stateFiltered[a].first_name; // give values to the variables
-		let surname = stateFiltered[a].last_name;
-		let middleName = stateFiltered[a].middle_name || " "; // means or else 
-		let party = stateFiltered[a].party;
-		let state = stateFiltered[a].state;
-		let seniority = stateFiltered[a].seniority;
-		let voteperc = stateFiltered[a].votes_with_party_pct;
-		let anchor = document.createElement("a"); //this creates the anchor elemen
-		let fullName = name + " " + middleName + " " + surname;
-		let linkName = stateFiltered[a].url; // link variable finds the value
-		anchor.setAttribute("href", linkName);
-		anchor.setAttribute("target", "_blank");
-		anchor.innerHTML = fullName;
-		td.append(anchor); // used to join different values "conct"
-		td1.append(party);
-		td2.append(state);
-		td3.append(seniority);
-		td4.append(voteperc + "%");
-		tr.append(td, td1, td2, td3, td4);
-		tbody.append(tr);
-	}
-
-}
--- main2.js
+++ main2.js
@@ -1,45 +1,15 @@
-function table(filterArray) { //displays string data as a table by creating td & tr
-		let tbody = document.getElementById("tbody");
-		tbody.innerHTML=''; // the div .tbody displays empty 
-	//console.log(data);
-	for (var a = 0; a < filterArray.length; a++) { //loop the array
-		let tr = document.createElement("tr"); //define the new variables for fn
-		let td = document.createElement("td");
-		let td1 = document.createElement("td");
-		let td2 = document.createElement("td");
-		let td3 = document.createElement("td");
-		let td4 = document.createElement("td");
-		let name = filterArray[a].first_name; // give values to the variables
-		let surname = filterArray[a].last_name;
-		let middleName = filterArray[a].middle_name || " "; // means or else 
-		let party = filterArray[a].party;
-		let state = filterArray[a].state;
-		let seniority = filterArray[a].seniority;
-		let voteperc = filterArray[a].votes_with_party_pct;
-		let anchor = document.createElement("a"); //this creates the anchor elemen
-		// let fullName = document.createElement("fullName"); // creates element 
-		let fullName = name + " " + middleName + " " + surname;
-		let linkName = filterArray[a].url; // link variable finds the value
-		anchor.setAttribute("href", linkName);
-		anchor.setAttribute("target", "_blank");
-		anchor.innerHTML = fullName;
-		td.append(anchor); // used to join different values "conct"
-		td1.append(party);
-		td2.append(state);
-		td3.append(seniority);
-		td4.append(voteperc + "%");
-		tr.append(td, td1, td2, td3, td4);
-		tbody.append(tr);
-	}
+// Main funtion to be called for display information 
+states(); //allways called for the states to show on the dropdown filter
 
-}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------//
 
-function states() {
+
+function states() { // function to display all the states on the filter dropdown list 
 	let statesArray = []; // new empty array to put all the states fom "let state" variable 
 	let select = document.getElementById("selectOpt"); // creates an element 
 	for (var a = 0; a < data.results[0].members.length; a++) {
 		let state = data.results[0].members[a].state;
-		statesArray.push(state); //all the states are on one array 
+		statesArray.sort().push(state); //all the states are on one array 
 		var repeatedValues = []; // new empty array to contain all the values that are not repeated
 		for (var i = 0; i < statesArray.length; i++) { //loop to look all the states in the states.array
 			for (var j = i + 1; j < statesArray.length; j++) { // another loop to to check the data again  
@@ -54,36 +24,83 @@ function states() {
 	for (var ll = 0; ll < repeatedValues.length; ll++) { // new loop to append every state to the selection / option html
 		let option = document.createElement("option");
 		option.append(repeatedValues[ll]);
+
 		selectOpt.append(option);
 	}
 }
-states();
+//-------------------------------------------------------------------------------------------------------//
 
-//function newFunction(){
-//	
-//	return Array.from(new Set(data.results[0].members.map((member) =>{ member.state}).sort()));
-//	
-//}
+document.getElementById("selectOpt").addEventListener("change", filterAll); // event that listens to the filter drop down list and starts the function filterAll
 
-function partyFilter (){ //This function is to get all the checkboxes with query selector
-	let partyName=[];
-	let checkValue = document.querySelectorAll("input[name=partyName]:checked"); // gets the values of all checkboxes and stores them in partyName array 
+
+function filterAll() {
+	var members = data.results[0].members; // insted of writing everything I just write members.length etc for the loops 
+	let checkValue = [...document.querySelectorAll("input[name=partyName]:checked")]; // gets the values of all checkboxes and stores them
+	let parties = checkValue.map(checked => checked.value); // a function that filters every value of the checkboxes and stores the value in an array
+	let idVal = document.getElementById("selectOpt"); // connect the variable to the value of the html checkbox "value=D,R,I" 
+	let statesF = idVal.value; // the value of the filter by state
+	let filteredMembers = []; // main array that gets filtered by the loops and used to store elements every time a condition is met
 	
-	for (let i=0; i < checkValue.length; i++){
-		partyName.push(checkValue[i].value);
+	for (var i = 0; i < members.length; i++) { // main loop that is going to be used by every if(conditions are met / not )
+		if ((statesF == "All") && (parties.length == 0)) { // if the states dropdown has no value and no checkboxes checked condition
+			filteredMembers.push(members[i]); // Loop everything and push all members to the main array
+		} else if ((members[i].state == statesF) && (parties.length == 0)) { // if members state is true and no checkboxes are checked 
+			filteredMembers.push(members[i]);
+		} else if ((statesF == "All") && (parties.length > 0)) { // if state is false and the checkboxes are checked 
+			for (var i = 0; i < members.length; i++) { // loop through all the members 
+				for (var a = 0; a < parties.length; a++) {// loop to filter all the members by the checkbox value  
+					if (members[i].party === parties[a]) { // if members party is equal to the value of checkbox 
+						filteredMembers.push(members[i]); // push everything to the array 
+					}
+				}
+			}
+		} else if ((statesF == members[i].state) && (parties.length > 0)) { // if state value is true and checkboxes checked 
+			for (var i = 0; i < members.length; i++) { 
+				for (var a = 0; a < parties.length; a++) { //loop through the value of the checkboxes
+					if ((members[i].party == parties[a]) && (members[i].state == statesF)) { //if value of states and value of party is equal to the member
+						filteredMembers.push(members[i]);
+					}
+				}
+			}
+		}
+		table(filteredMembers); // call the table with this array to print the results 
 	}
-	partyFilterTable(partyName); // thats how u call a previous function to get all the var.
 }
+	filterAll(); // Needs to be called to print all the members without filters 
 
-function partyFilterTable (partyname) {
-	let filterParty = [];
-	 for (var i=0; i < data.results[0].members.length; i++){
-		 for (var a=0; a < partyname.length; a++) {
-			 if ( data.results[0].members[i].party === partyname[a]) {
-				filterParty.push(data.results[0].members[i]);
-			 }
-		 }
-	 }
-	console.log("filterPArty", filterParty)
-	table(filterParty); // call the table function to generate a new table with the value of the checkboxes
-}
+	//-------------------------------------------------------------------------------------------------------//
+
+
+	function table(stateFiltered) { //displays string data as a table by creating td & tr
+		let tbody = document.getElementById("tbody");
+		tbody.innerHTML = ''; // the div .tbody displays empty 
+		for (var a = 0; a < stateFiltered.length; a++) { //loop the array
+			let tr = document.createElement("tr"); //define the new variables for fn
+			let td = document.createElement("td");
+			let td1 = document.createElement("td");
+			let td2 = document.createElement("td");
+			let td3 = document.createElement("td");
+			let td4 = document.createElement("td");
+			let name = stateFiltered[a].first_name; // give values to the variables
+			let surname = stateFiltered[a].last_name;
+			let middleName = stateFiltered[a].middle_name || " "; // means or else 
+			let party = stateFiltered[a].party;
+			let state = stateFiltered[a].state;
+			let seniority = stateFiltered[a].seniority;
+			let voteperc = stateFiltered[a].votes_with_party_pct;
+			let anchor = document.createElement("a"); //this creates the anchor elemen
+			let fullName = name + " " + middleName + " " + surname;
+			let linkName = stateFiltered[a].url; // link variable finds the value
+			anchor.setAttribute("href", linkName);
+			anchor.setAttribute("target", "_blank");
+			anchor.innerHTML = fullName;
+			td.append(anchor); // used to join different values "conct"
+			td1.append(party);
+			td2.append(state);
+			td3.append(seniority);
+			td4.append(voteperc + "%");
+			tr.append(td, td1, td2, td3, td4);
+			tbody.append(tr);
+		}
+
+	}
